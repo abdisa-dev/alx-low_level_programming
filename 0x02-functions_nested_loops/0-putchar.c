a7fd9bc706d74bc3c86442101a4f4f628b169d89
Abdisa Dev <abdisadev@gmail.com>
2023-08-16 06:20:56 +0300

    main header file and putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print every single char in variable a
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+    char a[] = "_putchar";
+
+    int b;
+
+    for (b = 0; b < 8; b++)
+    {
+        _putchar(a[b]);
+    }
+    _putchar('\n');
+    return (0);
+}
