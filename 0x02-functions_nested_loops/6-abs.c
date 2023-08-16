7ae1e7ea3e1988be08f4bcd21d65e28efae602e6
Abdisa Dev <abdisadev@gmail.com>
2023-08-16 22:50:25 +0300

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - return absolute value of input
+ * Return: absolute values of input
+ * @n: user input
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return n;
+}
