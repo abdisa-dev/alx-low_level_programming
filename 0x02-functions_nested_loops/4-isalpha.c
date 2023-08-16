320a3dd2c295de185c63f8521babccec935c597d
Abdisa Dev <abdisadev@gmail.com>
2023-08-16 22:32:21 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
- *
- *
+ * _isalpha - return all alpahabets
+ * Return: 1 depend on the value of c
+ * @c: charachter that recives input
  */
 
 int _isalpha(int c)
 {
-    if ((c <= 65 && c >= 90) || (c <= 97 && c >= 122))
-    {
-    }
+	if ((c <= 65 && c >= 90) || (c <= 97 && c >= 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
