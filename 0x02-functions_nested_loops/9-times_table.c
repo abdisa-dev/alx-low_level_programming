2728d1d0eed91111ce3ae9fc9805c43f2552245d
Abdisa Dev <abdisadev@gmail.com>
2023-08-20 18:53:53 +0300

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,11 +16,14 @@ void times_table(void)
 
 			if (c <= 9)
 			{
+				if (c != 0)
+				{
+					_putchar(' ');
+				}
 				_putchar(c + '0');
 				if (b != 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
