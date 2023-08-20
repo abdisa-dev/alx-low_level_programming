ad7fedebce216f9e68ac3f67afb39e2c1fc05ccd
Abdisa Dev <abdisadev@gmail.com>
2023-08-20 21:08:26 +0300

    times table for the given input

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints input number times table
+ * @n: input number from the user
+ */
+
+void print_times_table(int n)
+{
+	int a, b, c;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			c = a * b;
+
+			if (c > 9 && c <= 15)
+			{
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else if (n != 0)
+			{
+				_putchar(c + '0');
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
