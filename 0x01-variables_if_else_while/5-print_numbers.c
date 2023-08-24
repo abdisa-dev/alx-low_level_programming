f1fd81c64781afad30c5f612d8adda85d4c7c94e
Abdisa Dev <abdisadev@gmail.com>
2023-08-24 07:35:00 +0300

    print decimal number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all decimal number
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf(a);
+	}
+	printf('\n');
+	return (0);
+}
