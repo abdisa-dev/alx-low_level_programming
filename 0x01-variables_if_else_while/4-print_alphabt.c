f49b4a7dff035ec4a6de1da482647c505b398ab7
Abdisa Dev <abdisadev@gmail.com>
2023-08-24 07:31:56 +0300

    print alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-    int a;
+	int a;
 
-    for (a = 'a'; a <= 'z'; a++)
-    {
-        if (a != 'e' && a != 'q')
-            putchar(a);
-    }
-    putchar('\n');
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+			putchar(a);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
