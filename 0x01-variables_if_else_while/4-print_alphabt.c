a4eb38d9c2f3d01e5459b92bd17c3e12dd14d5ed
Abdisa Dev <abdisadev@gmail.com>
2023-08-24 07:25:54 +0300

    positive and negative

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all lowercase alphabet except e and q
+ * Return: always 0
+ */
+
+int main(void)
+{
+    int a;
+
+    for (a = 'a'; a <= 'z'; a++)
+    {
+        if (a != 'e' && a != 'q')
+            putchar(a);
+    }
+    putchar('\n');
+
+    return (0);
+}
