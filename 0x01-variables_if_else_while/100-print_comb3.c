6f2486165f9d3928096de2f741a04386957dfd31
Abdisa Dev <abdisadev@gmail.com>
2023-08-25 06:40:29 +0300

    2 digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main -
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a <= '8'; a++)
+	{
+		for (b = '1'; b <= '9'; b++)
+		{
+			if (b > a)
+			{
+
+				putchar(a);
+				putchar(b);
+				if (b != '9' || a != '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
