0edb85dee3b83754c9231c9337b14b50c380764d
Abdisa Dev <abdisadev@gmail.com>
2023-08-27 08:23:18 +0300

    remaing days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -22,6 +22,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
+
 	else
 	{
 		if (month == 2 && day == 60)
@@ -30,6 +31,7 @@ void print_remaining_days(int month, int day, int year)
 		}
 		else
 		{
+
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
