1b3cca93c4b1e85a9977cbb45d872ab9c0f48f78
Abdiza Alemu <abdisadev>
2024-01-16 22:18:36 +0300

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print the first option
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: always 0
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
