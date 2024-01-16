f26ecf30bd8e57b1c79dab838ea4931872a5f9f6
Abdiza Alemu <abdisadev>
2024-01-16 22:33:48 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the length of passed arguments
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: always 0
+*/
+
+int main(int __attribute__((unused))argc, char __attribute__((unused))*argv[])
+{
+	printf("%d", argc - 1);
+
+	return (0);
+}
