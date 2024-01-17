4f8748abae0b619509f943166776c5976490ed07
Abdisa Alemu <abdisadev@gmail.com>
2024-01-17 08:14:18 +0300

    add from cli input

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,82 @@
-#include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
- * main - print the all passed arguments
- * @argc: argument counter
- * @argv: argument vector
- * Return: always 0 if valid argument
-*/
-
-int main(int __attribute__((unused))argc, char __attribute__((unused))*argv[])
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
 {
-	int i, j, sum;
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argc == 1)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("%d\n", 0);
-
-			return (0);
-		} else if (*argv[i] >= 48 && *argv[i] <= 57)
-		{
-			for (j = 0; j < (int)strlen(argv[i]); j++)
+			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
-				if (argv[i][j] > 57)
-				{
-					printf("Error\n");
-					return (1);
-				} else
-				{
-					sum += atoi(argv[i]);
-				}
+				puts("Error");
+				return (1);
 			}
-		} else
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+		if (num >= 0)
 		{
-			printf("Error\n");
-			return (1);
+			sum += num;
 		}
 	}
 
