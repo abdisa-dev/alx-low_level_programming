6b94b43b103d8facde02c9b2e04d4c0fb0a2d1d7
Abdiza Alemu <abdisadev>
2024-01-21 13:15:12 +0300

    dublicate str

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strdup - set a copy of str
+ * @str: string to be copied
+ * Return: address of copied str
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int counter = 0;
+	unsigned int str_len = strlen(str);
+	char *dup_str = malloc(sizeof(*str) * str_len);
+
+	if (str == NULL || dup_str == NULL)
+		return (NULL);
+	while (counter < str_len)
+	{
+		dup_str[counter] = *str++;
+		counter++;
+	}
+	dup_str[++counter] = '\0';
+	return (dup_str);
+}
