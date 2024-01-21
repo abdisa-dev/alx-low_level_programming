57686ce967f83ba90228a28a39a84fce2b37448b
Abdiza Alemu <abdisadev>
2024-01-21 12:43:55 +0300

    create arr

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,17 @@
 char *create_array(unsigned int size, char c)
 {
 	char *a;
-	unsigned int counter;
+	unsigned int i;
 
-	a = malloc(size * sizeof(c));
-
-	if (size <= 0 || a == NULL)
+	if (size == 0)
 		return (NULL);
 
-	while (size > counter)
-	{
-		a[counter] = c;
-		counter++;
-	}
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		a[i] = c;
 
 	return (a);
 }
